12e81b437a18264f162aeb789ec1c71296c2d54b
CPrimbee <cprimbee@gmail.com>
2020-01-30 17:55:21 -0300

    first commit

---
+++ PaymentContext.Tests/Entities/StudentTests.cs
@@ -0,0 +1,16 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PaymentContext.Domain.Entities;
+using PaymentContext.Domain.Enums;
+using PaymentContext.Domain.ValueObjects;
+
+namespace PaymentContext.Tests
+{
+    [TestClass]
+    public class StudentTests
+    {
+        //[TestMethod]
+        public void AdicionarAssinatura()
+        {
+        }
+    }
+}
---
+++ PaymentContext.Tests/ValueObjects/DocumentTests.cs
@@ -0,0 +1,50 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PaymentContext.Domain.Entities;
+using PaymentContext.Domain.Enums;
+using PaymentContext.Domain.ValueObjects;
+
+namespace PaymentContext.Tests
+{
+    [TestClass]
+    public class DocumentTests
+    {
+        // Red, Green, Refactor
+
+        [TestMethod]
+        public void ShouldReturnErrorWhenCNPJIsInvalid()
+        {
+            var doc = new Document("123", EDocumentType.CNPJ);
+            Assert.IsTrue(doc.Invalid);
+        }
+
+        [TestMethod]
+        [DataTestMethod]
+        [DataRow("56524481000199")]
+        [DataRow("15558603000180")]
+        [DataRow("86349076000142")]
+        public void ShouldReturnSuccessWhenCNPJIsValid(string cnpj)
+        {
+            var doc = new Document(cnpj, EDocumentType.CNPJ);
+            Assert.IsTrue(doc.Valid);
+        }
+
+        [TestMethod]
+        public void ShouldReturnErrorWhenCPFIsInvalid()
+        {
+            var doc = new Document("123", EDocumentType.CPF);
+            Assert.IsTrue(doc.Invalid);
+        }
+
+        [TestMethod]
+        [DataTestMethod]
+        [DataRow("85302219077")]
+        [DataRow("62415292001")]
+        [DataRow("38011345044")]
+        public void ShouldReturnSuccessWhenCPFIsValid(string cpf)
+        {
+            var doc = new Document(cpf, EDocumentType.CPF);
+            Assert.IsTrue(doc.Valid);
+        }
+
+    }
+}
